2db8be4bad612d1ba82f435db61c40af4c66bbbf
PenchalaVineeth <penchala.vineeth3@gmail.com>
2026-04-12 10:22:43 +0000

    first commit

---
+++ tourism_project/deployment/app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+from huggingface_hub import hf_hub_download
+import joblib
+
+# Download the model from the Model Hub
+@st.cache_resource
+def load_model():
+  """
+  Downloads the trained model from Hugging Face Hub and caches it 
+  to optimize performance by avoiding redundant file I/O.
+  """
+  model_path = hf_hub_download(
+      repo_id = 'vineeth32/tourism-model',
+      filename = 'best_tourism_model.joblib'
+      )
+  # Load the model
+  model = joblib.load(model_path)
+  return model
+
+model = load_model()
+
+# Streamlit UI whether customer purchases a package or not
+st.title('Visit with Us: Customer Purchase Predictor')
+st.subheader('An internal application predicts whether the customer is likely to purchase a package or not based on customer details and interaction data.')
+st.write('**Enter the customer details here.**')
+
+# Collect user input data
+# Customer details
+Age = st.number_input("Age (Customer's age in years)", min_value=18, max_value=100, value=45)
+TypeofContact = st.selectbox('The method by which the customer was contacted', ['Self Enquiry', 'Company Invited '])
+CityTier = st.selectbox('The city category customer belongs based on development, population, and living standards', [1, 2, 3])
+Occupation = st.selectbox("Customer's occupation", ['Salaried', 'Small Business', 'Large Business', 'Free Lancer'])
+Gender = st.selectbox('Gender of the customer', ['Male', 'Female'])
+NumberOfPersonVisiting = st.number_input('Total number of people accompanying the customer on the trip', min_value=0, max_value=10, value=2)
+PreferredPropertyStar = st.selectbox('Preferred hotel rating by the customer', [3, 4, 5])
+MaritalStatus = st.selectbox('Marital status of the customer', ['Married', 'Divorced', 'Unmarried', 'Single'])
+NumberOfTrips = st.number_input('Average number of trips the customer takes annually', min_value=0, max_value=50, value=2)
+Passport = st.selectbox('Whether the customer holds a valid passport', ['Yes', 'No'])
+OwnCar = st.selectbox('Whether the customer owns a car', ['Yes', 'No'])
+NumberOfChildrenVisiting = st.number_input('Number of children below age 5 accompanying the customer', min_value=0, max_value=3, value=2)
+Designation = st.selectbox("Customer's designation in their current organization", ['Executive', 'Manager', 'Senior Manager', 'AVP', 'VP'])
+MonthlyIncome = st.number_input('Gross monthly income of the customer', min_value=1000, max_value=100000, value=50000)
+
+# Customer interaction data
+st.write('**Sales Interaction Summary**')
+PitchSatisfactionScore = st.selectbox("Score indicating the customer's satisfaction with the sales pitch", [1, 2, 3, 4, 5])
+ProductPitched = st.selectbox('The type of product pitched to the customer', ['Basic', 'Deluxe', 'Standard', 'Super Deluxe', 'King'])
+NumberOfFollowups = st.slider('Total number of follow-ups by the salesperson after the sales pitch', min_value=1, max_value=10, value=3)
+DurationOfPitch = st.number_input('Duration of the sales pitch delivered to the customer', min_value=3, max_value=200, value=20)
+
+# Assemble input data into a dataframe
+input_data = pd.DataFrame([{
+    'Age': Age,
+    'TypeofContact': TypeofContact,
+    'CityTier': CityTier,
+    'Occupation': Occupation,
+    'Gender': Gender,
+    'NumberOfPersonVisiting': NumberOfPersonVisiting,
+    'PreferredPropertyStar': PreferredPropertyStar,
+    'MaritalStatus': MaritalStatus,
+    'NumberOfTrips': NumberOfTrips,
+    'Passport': 1 if Passport == 'Yes' else 0,
+    'OwnCar': 1 if OwnCar == 'Yes' else 0,
+    'NumberOfChildrenVisiting': NumberOfChildrenVisiting,
+    'Designation': Designation,
+    'MonthlyIncome': MonthlyIncome,
+    'PitchSatisfactionScore': PitchSatisfactionScore,
+    'ProductPitched': ProductPitched,
+    'NumberOfFollowups': NumberOfFollowups,
+    'DurationOfPitch': DurationOfPitch
+}])
+
+# Set the classification threshold
+classification_threshold = 0.45
+
+# Predict button
+if st.button('Predict'):
+  prediction_proba = model.predict_proba(input_data)[0, 1]
+  prediction = (prediction_proba >= classification_threshold).astype(int)
+  result = 'likely' if prediction == 1 else 'unlikely'
+  st.write(f"Based on the information provided the customer is '{result}' to purchase the package")
---
+++ tourism_project/model_building/train.py
@@ -0,0 +1,173 @@
+# Library for manipulating data
+import pandas as pd
+# Libraries for data preprocessing and creating model pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
+# Libraries for training, tuning and evaluation
+import xgboost as xgb
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+from sklearn.metrics import accuracy_score, classification_report, recall_score
+# Library to serialize model
+import joblib
+# Library for creating folder
+import os
+# Library for authenticating hugging face and to upload files
+from huggingface_hub import login, HfApi, create_repo
+from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
+# Library for tracking results and parameters
+import mlflow
+
+mlflow.set_tracking_uri('http://localhost:5000')
+mlflow.set_experiment('mlops-tourism-tracking')
+
+# Initialize API client
+api = HfApi()
+
+Xtrain_path = 'hf://datasets/vineeth32/tourism-data/Xtrain.csv'
+Xtest_path = 'hf://datasets/vineeth32/tourism-data/Xtest.csv'
+ytrain_path = 'hf://datasets/vineeth32/tourism-data/ytrain.csv'
+ytest_path = 'hf://datasets/vineeth32/tourism-data/ytest.csv'
+
+Xtrain = pd.read_csv(Xtrain_path)
+Xtest = pd.read_csv(Xtest_path)
+ytrain = pd.read_csv(ytrain_path)
+ytest = pd.read_csv(ytest_path)
+
+# Define target variable
+target = 'ProdTaken'
+
+# List of numerical features
+numeric_features = [
+    'Age',                        # Age of the customer
+    'NumberOfPersonVisiting',     # Total number of people accompanying the customer on the trip
+    'PreferredPropertyStar',      # Preferred hotel rating by the customer
+    'NumberOfTrips',              # Average number of trips the customer takes annually
+    'NumberOfChildrenVisiting',   # Number of children below age 5 accompanying the customer
+    'MonthlyIncome',              # Gross monthly income of the customer
+    'PitchSatisfactionScore',     # Score indicating the customer's satisfaction with the sales pitch
+    'NumberOfFollowups',          # Total number of follow-ups by the salesperson after the sales pitch
+    'DurationOfPitch'             # Duration of the sales pitch delivered to the customer
+]
+
+# List of categorical features
+categorical_features = [
+    'CityTier',        # The city category based on development, population, and living standards (Tier 1 > Tier 2 > Tier 3)
+    'TypeofContact',   # The method by which the customer was contacted (Company Invited or Self Inquiry)
+    'Occupation',      # Customer's occupation (e.g., Salaried, Freelancer)
+    'Gender',          # Gender of the customer (Male, Female)
+    'MaritalStatus',   # Marital status of the customer (Single, Married, Divorced)
+    'Passport',        # Whether the customer holds a valid passport (0: No, 1: Yes)
+    'OwnCar',          # Whether the customer owns a car (0: No, 1: Yes)
+    'Designation',     # Customer's designation in their current organization
+    'ProductPitched'   # The type of product pitched to the customer
+]
+
+# Set class weight to handle class imbalance
+class_weight = ytrain.value_counts()[0] / ytrain.value_counts()[1]
+
+# Define preprocessing steps
+preprocessor = make_column_transformer(
+    (StandardScaler(), numeric_features),
+    (OneHotEncoder(handle_unknown='ignore'), categorical_features)
+)
+
+# Define xgb model
+xgb_model = xgb.XGBClassifier(scale_pos_weight=class_weight, random_state=42)
+
+# Define Hyperparameter grid
+param_grid = {
+    'xgbclassifier__n_estimators': [50, 75, 100],    # number of tree to build
+    'xgbclassifier__max_depth': [2, 3, 4],    # maximum depth of each tree
+    'xgbclassifier__colsample_bytree': [0.4, 0.5, 0.6],    # percentage of attributes to be considered (randomly) for each tree
+    'xgbclassifier__colsample_bylevel': [0.4, 0.5, 0.6],    # percentage of attributes to be considered (randomly) for each level of a tree
+    'xgbclassifier__learning_rate': [0.01, 0.05, 0.1],    # learning rate
+    'xgbclassifier__reg_lambda': [0.4, 0.5, 0.6],    # L2 regularization factor
+}
+
+# Model pipeline
+model_pipeline = make_pipeline(preprocessor, xgb_model)
+
+# Scoring type to used to compare parameter combinations
+scorer = metrics.make_scorer(metrics.precision_score)
+
+with mlflow.start_run():
+  # Hyperparameter tuning
+  grid_search = GridSearchCV(model_pipeline, param_grid, cv=5, scoring=scorer, n_jobs=-1)
+  grid_search.fit(Xtrain, ytrain)
+
+  # Log all parameter combinations with their mean test scores
+  results = grid_search.cv_results_
+  for i in range(len(results['params'])):
+    param_set = results['params'][i]
+    mean_score = results['mean_test_score'][i]
+    std_score = results['std_test_score'][i]
+
+    # Log each combination as seperate mlflow run
+    with mlflow.start_run(nested=True):
+      mlflow.log_params(param_set)
+      mlflow.log_metric('mean_test_score', mean_score)
+      mlflow.log_metric('std_test_score', std_score)
+
+  # Log best parameters seperately in main run
+  mlflow.log_params(grid_search.best_params_)
+
+  # Store and evaulate best model
+  best_model = grid_search.best_estimator_
+
+  # Predict on train and test data
+  y_pred_train = best_model.predict(Xtrain)
+  y_pred_test = best_model.predict(Xtest)
+
+  # Probability predictions for precision
+  y_pred_train_proba = best_model.predict_proba(Xtrain)[:, 1]
+  y_pred_test_proba = best_model.predict_proba(Xtest)[:, 1]
+
+  # Generate train and test report
+  train_report = classification_report(ytrain, y_pred_train, output_dict=True)
+  print('Traning report:\n', train_report)
+  print('')
+  test_report = classification_report(ytest, y_pred_test, output_dict=True)
+  print('Test report:\n', test_report)
+
+  mlflow.log_metrics({
+      'train_accuracy': train_report['accuracy'],
+      'train_precision': train_report['1']['precision'],
+      'train_recall': train_report['1']['recall'],
+      'train_f1-score': train_report['1']['f1-score'],
+      'test_accuracy': test_report['accuracy'],
+      'test_precision': test_report['1']['precision'],
+      'test_recall': test_report['1']['recall'],
+      'test_f1-score': test_report['1']['f1-score']
+  })
+
+  # Save the best model locally
+  model_path = 'best_tourism_model.joblib'
+  joblib.dump(best_model, model_path)
+
+  # Log the model artifact
+  mlflow.log_artifact(model_path, artifact_path='model')
+  print(f'Model saved as artifact at: {model_path}')
+
+  # Upload to Hugging face
+  repo_id = 'vineeth32/tourism-model'
+  repo_type = 'model'
+
+  # Check if the space exists
+  try:
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
+    print(f"Space '{repo_id}' already exists. Using it.")
+  except RepositoryNotFoundError:
+    print(f"Space '{repo_id}' not found. Creating new space.")
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    print(f"Space '{repo_id}' created.")
+
+  # Upload best model to the model space created
+  api.upload_file(
+      path_or_fileobj='best_tourism_model.joblib',
+      path_in_repo='best_tourism_model.joblib',
+      repo_id=repo_id,
+      repo_type=repo_type
+  )
